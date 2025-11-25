20944d47a21f44a539610ae60b31af4ff325698b
Johncitoo <juan.contreras03@alumnos.ucn.cl>
2025-11-25 03:52:46 -0300

    revert: eliminar módulo de migraciones temporal

--- src/migrations/migrations.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MigrationsController } from './migrations.controller';
-
-@Module({
-  controllers: [MigrationsController],
-})
-export class MigrationsModule {}
