adb85097d2ab86d4afcb3ac873265a7cddd3edb0
Johncitoo <juan.contreras03@alumnos.ucn.cl>
2025-12-11 00:43:07 -0300

    fix: Importar AuthModule en CallsModule para resolver dependencias de JwtAuthGuard

--- src/calls/calls.module.ts
+++ src/calls/calls.module.ts
@@ -5,9 +5,13 @@ import { CallsService } from './calls.service';
 import { Call, FormSection, FormField } from './entities';
 import { Form } from '../forms/entities/form.entity';
 import { Milestone } from '../milestones/entities/milestone.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Call, FormSection, FormField, Form, Milestone])],
+  imports: [
+    TypeOrmModule.forFeature([Call, FormSection, FormField, Form, Milestone]),
+    AuthModule,
+  ],
   controllers: [CallsController],
   providers: [CallsService],
   exports: [CallsService],
