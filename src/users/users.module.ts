4f16d165bcb78c76c7cf00cd94396606bc8e9376
Johncitoo <juan.contreras03@alumnos.ucn.cl>
2025-12-16 22:18:59 -0300

    fix: Agregar Admin2FACode entity a TypeORM

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -9,13 +9,14 @@ import { UserAuthController } from './user-auth.controller';
 import { AdminManagementController } from './admin-management.controller';
 import { User } from './entities/user.entity';
 import { AdminVerificationCode } from './entities/admin-verification-code.entity';
+import { Admin2FACode } from './entities/admin-2fa-code.entity';
 import { AdminCreationService } from './admin-creation.service';
 import { Admin2FAService } from './admin-2fa.service';
 import { EmailModule } from '../email/email.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, AdminVerificationCode]),
+    TypeOrmModule.forFeature([User, AdminVerificationCode, Admin2FACode]),
     JwtModule,
     ConfigModule,
     EmailModule,
