78972b6637649f266532d7d8c11efe496cb4a5be
Cristian Urqueta <cristianurqueta23@gmail.com>
2025-12-16 21:19:08 -0300

    revert: Volver configuración original de UsersModule (login funcionaba)

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,6 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
@@ -9,14 +10,13 @@ import { User } from './entities/user.entity';
 import { Admin2FACode } from './entities/admin-2fa-code.entity';
 import { Admin2FAService } from './admin-2fa.service';
 import { EmailModule } from '../email/email.module';
-import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, Admin2FACode]),
+    JwtModule,
     ConfigModule,
     EmailModule,
-    forwardRef(() => AuthModule),
   ],
   controllers: [UsersController, AdminUsersController, UserAuthController],
   providers: [UsersService, Admin2FAService],
